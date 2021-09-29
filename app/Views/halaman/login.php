13803b64e47ff50c33e6964180a23c0d17302759
12200907siskasera <siskasera09@gmail.com>
2021-09-28 23:50:57 -0700

    tugas pertemuan 2

---
+++ app/Views/halaman/login.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Perpustakaan Login</title>
+    </head>
+    <body>
+        <h1>Selamat datang di Sistem Informasi Perpustakan</h1>
+        <p>
+            Ini adalah layanan perpustakaan digital masa kini
+            <a href="<?=base_url('/daftar-member')?>">klik daftar</a>
+        </p>
+    </body>
+
+</html>
